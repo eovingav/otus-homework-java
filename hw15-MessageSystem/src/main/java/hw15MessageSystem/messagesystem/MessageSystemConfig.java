034531c7f900961ec4c205cfb0d25471ed0c2f08
Гниломёдова А.В <a.gnilomedova@centrofinans.ru>
2020-01-07 23:42:07 +0100

    Изменена инициалиализация MessageSystem

--- hw15-MessageSystem/src/main/java/hw15MessageSystem/messagesystem/MessageSystemConfig.java
+++ hw15-MessageSystem/src/main/java/hw15MessageSystem/messagesystem/MessageSystemConfig.java
@@ -1,21 +1,12 @@
 package hw15MessageSystem.messagesystem;
 
-import hw15MessageSystem.db.handlers.UserAddRequestHandler;
-import hw15MessageSystem.db.handlers.UserAuthRequestHandler;
-import hw15MessageSystem.db.handlers.UserListRequestHandler;
-import hw15MessageSystem.db.repository.UsersRepository;
 import hw15MessageSystem.frontend.FrontendService;
 import hw15MessageSystem.frontend.FrontendServiceImpl;
-import hw15MessageSystem.frontend.handlers.UserAddResponseHandler;
-import hw15MessageSystem.frontend.handlers.UserAuthResponseHandler;
-import hw15MessageSystem.frontend.handlers.UserListResponseHandler;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
 
 @Configuration
 @ComponentScan
@@ -24,33 +15,28 @@ public class MessageSystemConfig {
     @Autowired
     private final MessageSystem messageSystem;
 
-    @Autowired
-    private final UsersRepository usersRepository;
-
     private final String DATABASE_SERVICE_CLIENT_NAME = "db";
     private final String FRONTEND_SERVICE_CLIENT_NAME = "frontend";
 
-    public MessageSystemConfig(MessageSystem messageSystem, UsersRepository usersRepository) {
+    public MessageSystemConfig(MessageSystem messageSystem) {
         this.messageSystem = messageSystem;
-        this.usersRepository = usersRepository;
     }
 
     @Bean
-    public FrontendService frontendService(){
-        MsClient databaseMsClient = new MsClientImpl(DATABASE_SERVICE_CLIENT_NAME, messageSystem);
-        databaseMsClient.addHandler(MessageType.USER_AUTH, new UserAuthRequestHandler(usersRepository));
-        databaseMsClient.addHandler(MessageType.USER_LIST, new UserListRequestHandler(usersRepository));
-        databaseMsClient.addHandler(MessageType.USER_ADD, new UserAddRequestHandler(usersRepository));
-        messageSystem.addClient(databaseMsClient);
+    @Qualifier("databaseMsClient")
+    public MsClient databaseMsClient(){
+        return new MsClientImpl(DATABASE_SERVICE_CLIENT_NAME, messageSystem);
+    }
 
-        MsClient frontendMsClient = new MsClientImpl(FRONTEND_SERVICE_CLIENT_NAME, messageSystem);
-        FrontendService frontendService = new FrontendServiceImpl(frontendMsClient, DATABASE_SERVICE_CLIENT_NAME);
-        frontendMsClient.addHandler(MessageType.USER_AUTH, new UserAuthResponseHandler(frontendService));
-        frontendMsClient.addHandler(MessageType.USER_LIST, new UserListResponseHandler(frontendService));
-        frontendMsClient.addHandler(MessageType.USER_ADD, new UserAddResponseHandler(frontendService));
-        messageSystem.addClient(frontendMsClient);
+    @Bean
+    @Qualifier("frontendMsClient")
+    public MsClient frontendMsClient(){
+        return new MsClientImpl(FRONTEND_SERVICE_CLIENT_NAME, messageSystem);
+    }
 
-        return frontendService;
+    @Bean
+    public FrontendService frontendService( @Qualifier("frontendMsClient") MsClient frontendMsClient) {
+        return new FrontendServiceImpl(frontendMsClient, DATABASE_SERVICE_CLIENT_NAME);
     }
 
 }
